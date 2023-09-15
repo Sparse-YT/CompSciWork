e6213005d3c8086adb7ac3a84110a6efd4e19044
Sparse-YT <144548317+Sparse-YT@users.noreply.github.com>
2023-09-15 08:39:27 -0400

    Update DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -5,16 +5,16 @@ public class DecimalToBinary {
            try (Scanner keyboard = new Scanner(System.in)) {
                System.out.print("Please enter in a number between 0 and 255 | ");
                int decimal = keyboard.nextInt();
-               int h = (decimal / 255) % 2;
-               int g = (decimal / 128) % 2;
-               int f = (decimal / 64) % 2;
-               int e = (decimal / 32) % 2;
-               int d = (decimal / 16) % 2;
-               int c = (decimal / 8) % 2;
-               int b = (decimal / 4) % 2;
+               int h = (decimal / 128) % 2;
+               int g = (decimal / 64) % 2;
+               int f = (decimal / 32) % 2;
+               int e = (decimal / 16) % 2;
+               int d = (decimal / 8) % 2;
+               int c = (decimal / 4) % 2;
+               int b = (decimal / 2) % 2;
                int a = decimal % 2;
                
-               
+               System.out.print(h);
                System.out.print(g);
                System.out.print(f);
                System.out.print(e);
@@ -22,7 +22,7 @@ public class DecimalToBinary {
                System.out.print(c);
                System.out.print(b);
                System.out.print(a);
-               System.out.print(h);
+               
 
                 
 
