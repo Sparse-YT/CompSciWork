2e36073211c036d1ce99e9b2358328f2d613d7a3
Sparse-YT <144548317+Sparse-YT@users.noreply.github.com>
2023-10-03 19:39:53 -0400

    Add files via upload

---
+++ Zookeeper/src/Zookeeper.java
@@ -0,0 +1,36 @@
+
+
+
+public class Zookeeper{
+    
+                            public static void main (String [] args) {
+                    
+
+                                
+
+                             Lion leo = new Lion (2, 100.2, "leo", false);
+                                 leo.roar();
+                             Lion kimba = new Lion( 1, 70.5,"kimba", false);
+                                 kimba.list();
+                             Lion kyle = new Lion( 1, 80.4, "kyle", false );
+                                   kyle.giveName(kyle);
+                             Lion ben = new Lion( 5, 250.3, "ben", true);
+                                    ben.death();
+                                 
+                                    
+
+
+
+                            } 
+                        }
+                            
+
+
+                          
+ 
+                            
+                        
+                    
+
+
+
